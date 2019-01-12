3a0b49c12d64109e504c33b6e57daf5792c681af
mojtaba <shafaei.saeid@gmail.com>
2019-01-12 18:49:37 +0330

    fix some errors

--- app/src/main/java/com/mojtaba_shafaei/android/android_toggle_button_example/MainActivity.java
+++ app/src/main/java/com/mojtaba_shafaei/android/android_toggle_button_example/MainActivity.java
@@ -9,8 +9,8 @@ import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
-import com.mojtaba_shafaei.android.library.ToggleButton;
+import android.widget.Toast;
+import com.mojtaba_shafaei.androidToggleButton.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,49 +20,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Typeface typeface = Typeface.createFromAsset(getAssets(), "AlfaSlabOne-Regular.ttf");
 
-        findViewById(R.id.btn_menu_more).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.btn_menu_more).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+            startActivity(intent);
         });
 
         ToggleButton toggleButton = findViewById(R.id.tb3);
         toggleButton.setTitleTypeface(typeface);
         toggleButton.setErrorEnabled(true);
         toggleButton.setError(R.string.errorText);
+        toggleButton.setOnCheckedChangeListener(button -> {
+            if (toggleButton.getValue() ==null) {
+                Toast.makeText(this, "non button checked", Toast.LENGTH_SHORT).show();
+            } else if (toggleButton.getValue().equals(0)) {
+                Toast.makeText(this, "button value = 0", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "button value = 1", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         final View root = findViewById(R.id.root);
 
-        findViewById(R.id.btnRtl).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ViewCompat.setLayoutDirection(root, ViewCompat.LAYOUT_DIRECTION_RTL);
-            }
-        });
+        findViewById(R.id.btnRtl).setOnClickListener(view -> ViewCompat.setLayoutDirection(root, ViewCompat.LAYOUT_DIRECTION_RTL));
 
-        findViewById(R.id.btnLtr).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ViewCompat.setLayoutDirection(root, ViewCompat.LAYOUT_DIRECTION_LTR);
-            }
-        });
+        findViewById(R.id.btnLtr).setOnClickListener(view -> ViewCompat.setLayoutDirection(root, ViewCompat.LAYOUT_DIRECTION_LTR));
 
         final ToggleButton toggle = findViewById(R.id.toggle);
         toggle.setButtonAText("تجربه کاری");
         toggle.setButtonBText("رشته تحصیلی");
-        toggle.setOnCheckedChangeListener(new ToggleButton.OnCheckedChangeListener() {
-            @Override
-            public void onChecked(int button) {
-                Log.i("TAG", "onChecked: " + button + ",value=" + toggle.getValue());
-            }
-        });
+        toggle.setOnCheckedChangeListener(button -> Log.i("TAG", "onChecked: " + button + ",value=" + toggle.getValue()));
 
         Drawable drawable = ContextCompat.getDrawable(this, R.drawable.bgs_button);
         Drawable drawable2 = ContextCompat.getDrawable(this, R.drawable.bgs_button);
-       // toggle.setButtonACompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
-       // toggle.setButtonBCompoundDrawablesWithIntrinsicBounds(drawable2, null, null, null);
+        // toggle.setButtonACompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+        // toggle.setButtonBCompoundDrawablesWithIntrinsicBounds(drawable2, null, null, null);
 
         findViewById(R.id.btn_test_val).setOnClickListener(new View.OnClickListener() {
             @Override
--- library/src/main/java/com/mojtaba_shafaei/androidToggleButton/ToggleButton.java
+++ library/src/main/java/com/mojtaba_shafaei/androidToggleButton/ToggleButton.java
@@ -1,15 +1,11 @@
-package com.mojtaba_shafaei.android.library;
+package com.mojtaba_shafaei.androidToggleButton;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorRes;
-import android.support.annotation.DimenRes;
-import android.support.annotation.Dimension;
-import android.support.annotation.IntDef;
-import android.support.annotation.StringRes;
+import android.support.annotation.*;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -17,15 +13,13 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.mojtaba_shafaei.android.toggleButton.R;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import static com.mojtaba_shafaei.android.library.ToggleButton.ButtonBgEnum.BLUE;
-import static com.mojtaba_shafaei.android.library.ToggleButton.ButtonBgEnum.GREEN;
+import static com.mojtaba_shafaei.androidToggleButton.ToggleButton.ButtonsEnum.BUTTON_NON;
 
 public class ToggleButton extends LinearLayout {
 
@@ -35,12 +29,7 @@ public class ToggleButton extends LinearLayout {
     private android.widget.ToggleButton toggleButtonA, toggleButtonB;
     private boolean isMandatory = false;
 
-    private OnCheckedChangeListener checkedChangeListener = new OnCheckedChangeListener() {
-        @Override
-        public void onChecked(int button) {
-            //this is dummy
-        }
-    };
+    private OnCheckedChangeListener mCheckedChangeListener;
 
     /////////////////////////////////////////////////////////
     public ToggleButton(Context context) {
@@ -64,18 +53,18 @@ public class ToggleButton extends LinearLayout {
                 toggleButtonA.setChecked(false);
                 toggleButtonB.setChecked(false);
 
-                // setWeight(.5F, .5F);
+// setWeight(.5F, .5F);
 
             } else if (value == 1) {
                 toggleButtonA.setChecked(true);
                 toggleButtonB.setChecked(false);
 
-                // setWeight(.6F, .4F);
+// setWeight(.6F, .4F);
             } else if (value == 0) {
                 toggleButtonA.setChecked(false);
                 toggleButtonB.setChecked(true);
 
-                // setWeight(.4F, .6F);
+// setWeight(.4F, .6F);
             }
             toggleButtonA.requestLayout();
             toggleButtonB.requestLayout();
@@ -101,33 +90,12 @@ public class ToggleButton extends LinearLayout {
     }
 
     private void initWithAttrs(Context context, AttributeSet attrs) {
-        TypedArray a = null;
-        View root;
-
-        if (attrs == null) {
-            root = inflate(context, R.layout.inherited, this);
-
-        } else {
-            a = context.getTheme().obtainStyledAttributes(
-                    attrs,
-                    R.styleable.ToggleButton,
-                    0, 0);
-
-            final int ll = a.getInt(R.styleable.ToggleButton_tb_layoutDirection, 2);
-            int layout;
-            if (ll == 0) {
-                layout = R.layout.l_ltr;
-            } else if (ll == 1) {
-                layout = R.layout.l_rtl;
-            } else {
-                layout = R.layout.inherited;
-            }
-
-            root = inflate(context, layout, this);
-        }
-
+        View root = inflate(context, R.layout.inherited, this);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.ToggleButton,
+                0, 0);
         try {
-
             toggleButtonA = root.findViewById(R.id.toggleButtonA);
             toggleButtonB = root.findViewById(R.id.toggleButtonB);
 
@@ -137,40 +105,42 @@ public class ToggleButton extends LinearLayout {
             toggleButtonA.setChecked(false);
             toggleButtonB.setChecked(false);
 
-            toggleButtonA.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (null != value && value == 1) {
-                        if (isMandatory) {
-                            value = 1;
-                        } else {
-                            value = null;
-                            checkedChangeListener.onChecked(ButtonsEnum.BUTTON_NON);
-                        }
-                    } else {
+            toggleButtonA.setOnClickListener(v -> {
+                if (null != value && value == 1) {
+                    if (isMandatory) {
                         value = 1;
-                        checkedChangeListener.onChecked(ButtonsEnum.BUTTON_A);
+                    } else {
+                        value = null;
+                        if (mCheckedChangeListener != null) {
+                            mCheckedChangeListener.onChecked(BUTTON_NON);
+                        }
                     }
-                    refreshButtons();
+                } else {
+                    value = 1;
+                    if (mCheckedChangeListener != null) {
+                        mCheckedChangeListener.onChecked(ButtonsEnum.BUTTON_A);
+                    }
                 }
+                refreshButtons();
             });
 
-            toggleButtonB.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (null != value && value == 0) {
-                        if (isMandatory) {
-                            value = 0;
-                        } else {
-                            value = null;
-                            checkedChangeListener.onChecked(ButtonsEnum.BUTTON_NON);
-                        }
-                    } else {
+            toggleButtonB.setOnClickListener(v -> {
+                if (null != value && value == 0) {
+                    if (isMandatory) {
                         value = 0;
-                        checkedChangeListener.onChecked(ButtonsEnum.BUTTON_B);
+                    } else {
+                        value = null;
+                        if (mCheckedChangeListener != null) {
+                            mCheckedChangeListener.onChecked(BUTTON_NON);
+                        }
                     }
-                    refreshButtons();
+                } else {
+                    value = 0;
+                    if (mCheckedChangeListener != null) {
+                        mCheckedChangeListener.onChecked(ButtonsEnum.BUTTON_B);
+                    }
                 }
+                refreshButtons();
             });
 
             if (attrs == null) {
@@ -178,7 +148,7 @@ public class ToggleButton extends LinearLayout {
                 toggleButtonA.performClick();
             } else {
                 isMandatory = a.getBoolean(R.styleable.ToggleButton_tb_isMandatory, false);
-                value = a.getInteger(R.styleable.ToggleButton_tb_default_button, -1);
+                value = a.getInteger(R.styleable.ToggleButton_tb_default_button, BUTTON_NON);
                 switch (value) {
                     case 0:
                         toggleButtonB.performClick();
@@ -212,23 +182,6 @@ public class ToggleButton extends LinearLayout {
                 String tbb = a.getString(R.styleable.ToggleButton_tb_B_text);
                 setButtonBText(tbb);
 
-                if (a.hasValue(R.styleable.ToggleButton_tb_background_A)) {
-                    int buttonABackgroundResId = a.getResourceId(R.styleable.ToggleButton_tb_background_A,
-                            R.drawable.bg_toggle_selector);
-                    setBackgroundA(buttonABackgroundResId);
-                } else {
-                    int buttonAbgType = a.getInt(R.styleable.ToggleButton_tb_background_A_type, 1);
-                    setBackgroundAType(buttonAbgType);
-                }
-
-                if (a.hasValue(R.styleable.ToggleButton_tb_background_B)) {
-                    int buttonBBackgroundResId = a.getResourceId(R.styleable.ToggleButton_tb_background_B,
-                            R.drawable.bg_toggle_selector);
-                    setBackgroundB(buttonBBackgroundResId);
-                } else {
-                    int buttonBbgType = a.getInt(R.styleable.ToggleButton_tb_background_B_type, 1);
-                    setBackgroundBType(buttonBbgType);
-                }
 
                 toggleButtonA.requestLayout();
                 toggleButtonB.requestLayout();
@@ -249,41 +202,6 @@ public class ToggleButton extends LinearLayout {
 
     public void setBackgroundB(int buttonABackgroundResId) {
         toggleButtonB.setBackgroundResource(buttonABackgroundResId);
-    }
-
-
-    /**
-     * @deprecated use {@link #setBackgroundA(int)}
-     */
-    @Deprecated
-    public void setBackgroundAType(@ButtonBgEnum int type) {
-        switch (type) {
-            case BLUE:
-                toggleButtonA.setBackgroundResource(R.drawable.bg_toggle_selector);
-                break;
-
-            case GREEN:
-                toggleButtonA.setBackgroundResource(R.drawable.bg_toggle_green);
-                break;
-        }
-
-    }
-
-    /**
-     * @deprecated use {@link #setBackgroundB(int)}
-     */
-    @Deprecated
-    public void setBackgroundBType(@ButtonBgEnum int type) {
-        switch (type) {
-            case BLUE:
-                toggleButtonB.setBackgroundResource(R.drawable.bg_toggle_selector);
-                break;
-
-            case GREEN:
-                toggleButtonB.setBackgroundResource(R.drawable.bg_toggle_green);
-                break;
-        }
-
     }
 
     public synchronized Integer getValue() {
@@ -374,7 +292,8 @@ public class ToggleButton extends LinearLayout {
     /**
      * @param text      is {@link String}, may be empty or null.
      * @param typeface  is {@link Typeface}, may be null
-     * @param textColor is a {@link ColorStateList} for Checkable Components[state_checked should implement], may be {@code null}.
+     * @param textColor is a {@link ColorStateList} for Checkable Components[state_checked should implement], may be {@code
+     *                  null}.
      */
     public void setButtonAText(String text, ColorStateList textColor, Typeface typeface) {
         if (text != null) {
@@ -405,7 +324,8 @@ public class ToggleButton extends LinearLayout {
         this.toggleButtonA.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size);
     }
 
-    public void setButtonACompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom) {
+    public void setButtonACompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right,
+                                                               Drawable bottom) {
         toggleButtonA.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
     }
 
@@ -413,7 +333,8 @@ public class ToggleButton extends LinearLayout {
         toggleButtonA.setCompoundDrawables(left, top, right, bottom);
     }
 
-    public void setButtonBCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom) {
+    public void setButtonBCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right,
+                                                               Drawable bottom) {
         toggleButtonB.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
     }
 
@@ -448,23 +369,17 @@ public class ToggleButton extends LinearLayout {
     }
 
     public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
-        this.checkedChangeListener = listener;
+        this.mCheckedChangeListener = listener;
     }
 
     @Retention(RetentionPolicy.SOURCE)
     @Documented
-    @IntDef({ButtonsEnum.BUTTON_A, ButtonsEnum.BUTTON_B, ButtonsEnum.BUTTON_NON})
+    @IntDef({ButtonsEnum.BUTTON_A, ButtonsEnum.BUTTON_B, BUTTON_NON})
     public @interface ButtonsEnum {
 
         int BUTTON_A = 1;
         int BUTTON_B = 0;
-        int BUTTON_NON = -1;
-    }
-
-    public @interface ButtonBgEnum {
-
-        int BLUE = 1;
-        int GREEN = 2;
+        int BUTTON_NON = 2;
     }
 
     public interface OnCheckedChangeListener {
